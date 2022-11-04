24a49dcad3bdf66f9ad8eb11f4907a9c2a54389e
Oguzhan <oguzhanbaysal@outlook.com>
2022-11-04 17:34:30 +0300

    Typing Speed App

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>Coded by Oguzhan Baysal</p>
+      <a target="blank" href="https://github.com/oguzhan-baysal">
+        <img src="https://cdn.iconscout.com/icon/free/png-32/github-153-675523.png" alt="github" />
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+
+import wordSlice from './words/wordSlice.js';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    words: wordSlice,
   },
 });
